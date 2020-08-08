19087c494c1dc588047e5347a9336b055a4db326
baimenovbn <baibn@mail.ru>
2020-08-08 12:02:46 +0600

    Functionality of adding tests(add_delete_table)

--- add_delete_table/main.js
+++ add_delete_table/main.js
@@ -1,31 +1,32 @@
 let newId = 4;
-let newTest = {'name': null, 'id': null, 'result': null};
+let newTest = {'name': null, 'id': newId, 'result': null};
 
 let tests = [
     {'name':'Distillation 50%', 'id':'1', 'result':"43"},
     {'name':'Flash Point', 'id':'2', 'result':"61"},
     {'name':'Water By Karl Fischer', 'id':'3', 'result':"24"},
 ];
+const tBody = document.getElementById('tests-table');
 
-function fillTable() {
-    let tBody = document.getElementById('tests-table');
-    for (let person of tests) {
-        let row = `<tr scope="row" class="test-row-${person.id}" data-testid=${person.id}>
-                        <td>${person.name}</td>
-                        <td data-action="edit">${person.result}</td>
-                        <td>
-                            <button id="delete" class="btn btn-danger btn-sm">Delete</button> 
-                            <button id="save" class="btn btn-info btn-sm" disabled>Save</button>
-
-                            <button id="cancel" class="btn btn-danger btn-sm hidden">Cancel</button> 
-                            <button id="confirm" class="btn btn-primary btn-sm hidden">Confirm</button>
-                        </td>
-                    </tr>`;
-        tBody.insertAdjacentHTML('beforeend', row);
-    }
+for (let test of tests) {
+    addRow(test);
 }
 
-fillTable();
+function addRow(test) {
+    let row = `<tr scope="row" class="test-row-${test.id}" data-testid=${test.id}>
+                    <td>${test.name}</td>
+                    <td data-action="edit">${test.result}</td>
+                    <td>
+                        <button id="delete" class="btn btn-danger btn-sm">Delete</button> 
+                        <button id="save" class="btn btn-info btn-sm" disabled>Save</button>
+
+                        <button id="cancel" class="btn btn-danger btn-sm hidden">Cancel</button> 
+                        <button id="confirm" class="btn btn-primary btn-sm hidden">Confirm</button>
+                    </td>
+                </tr>`;
+    tBody.insertAdjacentHTML('beforeend', row);
+}
+
 
 // START OF Editing functionality of existing values
 function deleteTest(id, row) {
@@ -91,3 +92,35 @@ table.addEventListener('click', function(e) {
 });
 
 // END OF Editing functionality of existing values
+
+
+// Adding new tests
+const formWrapper = document.querySelector('.form-wrapper');
+
+document.getElementById('add-test').addEventListener('click', function(e) {
+    formWrapper.classList.toggle('hidden');
+})
+
+formWrapper.addEventListener('input', function(e) {
+    if (e.target.tagName === 'INPUT') {
+        newTest.result = e.target.value;
+    };
+    if (e.target.tagName === 'SELECT') {
+        newTest.name = e.target.value;
+    };
+})
+
+document.querySelector('#create-test').addEventListener('click', function(e) {
+    if (newTest.name == null || newTest.result == null || newTest.id == null) return;
+    addRow(newTest);
+    newTest.name = null;
+    newTest.id += 1;
+    newTest.result = null;
+    formWrapper.classList.add('hidden');
+    cleanFields();
+});
+
+function cleanFields() {
+    document.querySelector('#test-result').value = '';
+    document.querySelector('#test-name').value = '';
+}
